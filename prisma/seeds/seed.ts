d134434201a8c6e4f30a16add11294efcaf7152f
YockyMai <g.valera15102003@gmail.com>
2026-09-10 19:11:37 +0500

    fix: increase transaction timeout for db seeding

--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -4,7 +4,6 @@ import { PrismaClient } from '../../src/generated/prisma/client.js';
 import addInitData from './202609100342-add-init-data.js';
 import 'dotenv/config';
 
-console.log(process.env.DATABASE_URL);
 const connectionString = `${process.env.DATABASE_URL}`;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
@@ -14,9 +13,14 @@ const seeds = [addInitData];
 
 async function main() {
   for (const seed of seeds) {
-    await prisma.$transaction(async (tx) => {
-      await seed(tx as PrismaClient);
-    });
+    await prisma.$transaction(
+      async (tx) => {
+        await seed(tx as PrismaClient);
+      },
+      {
+        timeout: 60000,
+      },
+    );
   }
 }
 
